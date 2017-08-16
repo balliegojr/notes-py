1384b4353bae4f73dd6267879e0ff068f615973f
Ilson Roberto Balliego Junior <ilson.balliego@outlook.com>
2017-08-16 11:57:25 -0300

    Adding editor override

--- notes.py
+++ notes.py
@@ -1,13 +1,13 @@
 """Note manager.
 
 Usage:
-  notes.py (insert | new) <note>...
+  notes.py (insert | new) [--editor=<$editor>] <note>...
   notes.py (remove | del) <note>...
-  notes.py edit <note>...
+  notes.py edit [--editor=<$editor>] <note>...
   notes.py show <note>...
   notes.py (move | rename) <note> <dst>
   notes.py list
-  notes.py 
+  notes.py
 
   notes.py (-h | --help)
   notes.py --version
@@ -15,6 +15,7 @@ Usage:
 Options:
   -h --help             Show this screen.
   --version             Show version.
+  --editor $editor      force the use of an editor
 """
 
 import os
@@ -50,6 +51,7 @@ def list_notes():
     print("Notes")
     os.system("tree -C -l --noreport -- {1} | tail -n +2 | sed -E 's/\{0}(\\x1B\\[[0-9]+m)?( ->|$)/\\1\\2/g'".format(base_extension, base_path))
 
+
 def edit_note(note_path):
     path = os.path.join(base_path, note_path + base_extension)
 
@@ -76,6 +78,7 @@ def remove_note(note_path):
 
     clear_empty_dir(os.path.dirname(path))
 
+
 def rename_note(note_path, new_note_path):
     src = os.path.join(base_path, note_path + base_extension)
     dst = os.path.join(base_path, new_note_path + base_extension)
@@ -89,6 +92,7 @@ def rename_note(note_path, new_note_path):
     shutil.move(src, dst)
     clear_empty_dir(os.path.dirname(src))
 
+
 def check_basepath():
     if not os.path.exists(base_path):
         os.makedirs(base_path)
@@ -100,6 +104,7 @@ def clear_empty_dir(path):
 
     if len(os.listdir(path)) == 0:
         os.removedirs(path)
+
 
 def load_config():
     global base_path
@@ -125,6 +130,7 @@ def load_config():
     base_extension = config['NOTES']['extension']
     editor = config['NOTES']['editor']
 
+
 def main(args):
     load_config()
     check_basepath()
@@ -132,6 +138,10 @@ def main(args):
     note = args['<note>']
     if isinstance(note, list):
         note = ' '.join(note)
+
+    if args['--editor']:
+        global editor
+        editor = args['--editor']
 
     if args['list']:
         list_notes()
